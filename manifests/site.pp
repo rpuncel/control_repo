7e270e738b2206bab66f1ab5b5ce7ba30c02f099
rpuncel <rpuncel@gmail.com>
2019-08-31 16:12:56 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,6 +10,10 @@ node default {
 }
 node 'master.puppet.vm' {
   include role::master_server
+  file { */root/README':
+    ensure => file,
+    content => $fqdn,
+  }
 }
 node /^web/ {
   include role::app_server
