d5118f1d8bf9adbb4a1241e1d04c0c9e7e5bce93
rpuncel <rpuncel@gmail.com>
2019-08-31 18:05:40 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,6 +8,9 @@ node default {
     owner => 'root',
   }
 }
+node 'minetest.puppet.vm' {
+  include role::minecraft_server
+}
 node 'master.puppet.vm' {
   include role::master_server
   file { '/root/README':
