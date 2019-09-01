4051455bc85bcca5a2815d8a259913c5b9c0e974
rpuncel <rpuncel@gmail.com>
2019-08-31 18:30:29 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -19,7 +19,7 @@ class minecraft (
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => epp('minecraft/minecraft.service',{
+    source => epp('minecraft/minecraft.service.epp',{
       install_dir => $install_dir,
     })
   }
